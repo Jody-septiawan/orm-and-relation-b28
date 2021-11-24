a14ba923c63c3b4705756a017a78160a4cd95bca
Jody Septiawan <jody@Jodys-MacBook-Air.local>
2021-11-24 10:00:40 +0700

    udpate

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,27 +1,70 @@
-const { user } = require('../../models')
+const { user } = require('../../models');
 
 exports.addUsers = async (req, res) => {
-    try {
+  try {
+    await user.create(req.body);
 
-        await user.create(req.body)
-
-        res.send({
-            status: 'success',
-            message: 'Add user finished'
-        })
-    } catch (error) {
-        console.log(error)
-        res.send({
-            status: 'failed',
-            message: 'Server Error'
-        })
-    }
-}
+    res.send({
+      status: 'success',
+      message: 'Add user finished',
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.getUsers = async (req, res) => {
-    // code here
-}
+  try {
+    const users = await user.findAll({
+      order: [['id', 'DESC']],
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      status: 'success',
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
 
 exports.getUser = async (req, res) => {
-    // code here
-}
+  try {
+    const { id } = req.params;
+
+    const userData = await user.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['password', 'createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      status: 'success',
+      data: {
+        user: userData,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      status: 'failed',
+      message: 'Server Error',
+    });
+  }
+};
